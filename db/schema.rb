6507f2787c5569cdbcd08b4ea4bb2ea7b10407c9
Raffaellochka <greenligth2000@gmail.com>
2023-02-13 01:16:50 +0300

    Добавил модель региона и сделал миграцию

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_160230) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_12_221413) do
   create_table "countries", force: :cascade do |t|
     t.string "name"
   end
 
+  create_table "regions", force: :cascade do |t|
+    t.string "name"
+    t.integer "country_id", null: false
+    t.index ["country_id"], name: "index_regions_on_country_id"
+  end
+
+  add_foreign_key "regions", "countries"
 end
